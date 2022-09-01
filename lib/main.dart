d06bb85f8dc1fb07a80ef42c38ee7fc40a064119
Radwan <73945168+RadwanH@users.noreply.github.com>
2022-09-01 17:14:01 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: Text('Match Pics'),
+        backgroundColor: Colors.indigo.shade800,
+      ),
+      body: ImagePage(),
+    ),
+  ));
+}
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({Key? key}) : super(key: key);
+
+  @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  int leftImageNum = 1;
+  int rightImageNum = 2;
+  void changeImage() {
+    leftImageNum = Random().nextInt(8) + 1;
+    rightImageNum = Random().nextInt(8) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          leftImageNum == rightImageNum ? 'Congrats!!! ' : 'Try again',
+          style: TextStyle(
+            fontSize: 42.0,
+            color: Colors.white,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeImage();
+                    });
+                  },
+                  child: Image.asset('images/image-$leftImageNum.png'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeImage();
+                    });
+                  },
+                  child: Image.asset('images/image-$rightImageNum.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
